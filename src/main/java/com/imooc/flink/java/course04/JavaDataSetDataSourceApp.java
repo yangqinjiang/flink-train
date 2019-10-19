3df31bed935e117f4c8434ed7b81fcf191b805db
yangqinjiang <531553280@qq.com>
2019-10-19 11:55:01 +0800

    java,从csv文件读取数据,并打印

--- src/main/java/com/imooc/flink/java/course04/JavaDataSetDataSourceApp.java
+++ src/main/java/com/imooc/flink/java/course04/JavaDataSetDataSourceApp.java
@@ -1,6 +1,8 @@
 package com.imooc.flink.java.course04;
 
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,9 +14,52 @@ public class JavaDataSetDataSourceApp {
     public static void main(String[] args) throws Exception{
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         //formCollection(env);
-        readTextFile(env);
-        readTextDir(env);
+        //readTextFile(env);
+        //readTextDir(env);
+        csvFile(env);
     }
+    //从csv文件读取数据,并打印
+    private static void csvFile(ExecutionEnvironment env) throws Exception {
+        /** 文件内容示例:
+         * name,age,job
+         * Jorge,30,Developer
+         * Bob,32,Developer
+         */
+        String filePath = "c:\\flink\\people.csv";
+        //指定数据类型或者tuple元组
+        //并且不要解析第一行数据
+        //输出三列数据
+        env.readCsvFile(filePath).ignoreFirstLine().types(String.class,Integer.class,String.class).print();
+        //输出三列数据,includedFields对应csv文件的顺序号,如0->name,  1->age,  2->job
+        /** 文件内容示例:
+         * name,age,job
+         * Jorge,30,Developer
+         * Bob,32,Developer
+         */
+
+        env.readCsvFile(filePath)
+                .ignoreFirstLine()
+                .types(String.class,Integer.class,String.class)//输出所有
+                .print();
+        //输出csv文件的两列数据 name,job , includedFields = true,false,true
+        env.readCsvFile(filePath)
+                .ignoreFirstLine()
+                .includeFields(true,false,true)//忽略age列
+                .types(String.class,String.class)//对应上一行代码的true/false
+                .print();
+        //POJO,使用java的类,输出name,age,work
+        env.readCsvFile(filePath).ignoreFirstLine()
+                .pojoType(Person.class,"name","age","work").print();
+        //POJO,使用java的类,输出name,age
+        env.readCsvFile(filePath).ignoreFirstLine()
+                .pojoType(Person.class,"name","age").print();
+        /** output:
+         * Person{name='Bob', age=32, work=''}
+         * Person{name='Jorge', age=30, work=''}
+         */
+    }
+
+
     //从本地文件夹读取数据，并打印
     private static void readTextDir(ExecutionEnvironment env) throws Exception{
         String filePath = "c:\\flink\\input";
