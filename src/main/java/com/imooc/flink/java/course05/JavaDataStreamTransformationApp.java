bbea25802cbf53aa7b9c47171a408602b2ba1370
yangqinjiang <531553280@qq.com>
2019-10-20 17:35:39 +0800

    DataStream API: split,select的综合使用

--- src/main/java/com/imooc/flink/java/course05/JavaDataStreamTransformationApp.java
+++ src/main/java/com/imooc/flink/java/course05/JavaDataStreamTransformationApp.java
@@ -2,8 +2,14 @@ package com.imooc.flink.java.course05;
 
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.collector.selector.OutputSelector;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 数据流与算子
  */
@@ -11,9 +17,32 @@ public class JavaDataStreamTransformationApp {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 //        filterFunction(env);
-        unionFunction(env);
+//        unionFunction(env);
+        splitSelectFunction(env);
         env.execute("JavaDataStreamTransformationApp");
     }
+    //split, select 的综合使用
+    private static void splitSelectFunction(StreamExecutionEnvironment env) {
+        DataStreamSource<Long> data1 =  env.addSource(new CustomNonParallelSourceFunction());
+        //split数据 分解数据
+        SplitStream<Long> splits =  data1.split(new OutputSelector<Long>() {
+            @Override
+            public Iterable<String> select(Long value) {
+                List<String> output = new ArrayList<>();
+                if (value % 2 == 0){
+                    output.add("even-java");
+                }else{
+                    output.add("odd-java");
+                }
+                return output;  //返回,可迭代的数据
+            }
+        });
+        //select数据,输出split的odd数据
+        splits.select("odd-java").print().setParallelism(1);
+        //select数据,输出split的even数据
+        splits.select("even-java").print().setParallelism(1);
+    }
+
     //union
     private static void unionFunction(StreamExecutionEnvironment env) {
         DataStreamSource<Long> data1 =  env.addSource(new CustomNonParallelSourceFunction());
--- src/main/scala/com/imooc/flink/scala/course05/DataStreamTransformationApp.scala
+++ src/main/scala/com/imooc/flink/scala/course05/DataStreamTransformationApp.scala
@@ -1,5 +1,8 @@
 package com.imooc.flink.scala.course05
 
+import java.{lang, util}
+
+import org.apache.flink.streaming.api.collector.selector.OutputSelector
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 //隐式转换
 import org.apache.flink.api.scala._
@@ -12,8 +15,31 @@ object DataStreamTransformationApp {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     //filterFunction(env)
-    unionFunction(env)
+//    unionFunction(env)
+    splitSelectFunction(env)
     env.execute("DataStreamTransformationApp")
+  }
+  //split, select 的综合使用
+  def splitSelectFunction(env:StreamExecutionEnvironment)= {
+    //使用自定义的数据源,
+    val data1 = env.addSource(new CustomNonParallelSourceFunction())
+    //split数据 分解数据
+    val splits = data1.split(new OutputSelector[Long]{
+      override def select(value: Long): lang.Iterable[String] = {
+        //奇偶数
+        val output = new util.ArrayList[String]()
+        if (value % 2 == 0){
+          output.add("even")
+        }else{
+          output.add("odd")
+        }
+        output  //返回,可迭代的数据
+      }
+    });
+    //select数据,输出split的odd数据
+    splits.select("odd").print().setParallelism(1)
+    //select数据,输出split的even数据
+    splits.select("even").print().setParallelism(1)
   }
 
   //union
