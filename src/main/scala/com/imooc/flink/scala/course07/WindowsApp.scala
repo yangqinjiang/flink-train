75b627e5da72608431ee557cc0ebc337a8040201
yangqinjiang <531553280@qq.com>
2019-10-21 20:50:21 +0800

    time windows 的使用

---
+++ src/main/scala/com/imooc/flink/scala/course07/WindowsApp.scala
@@ -0,0 +1,28 @@
+package com.imooc.flink.scala.course07
+
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.time.Time
+
+/**
+ * time windows 的使用
+ * nc 的下载地址
+ * https://eternallybored.org/misc/netcat/
+ * 测试方式： nc -L -p 9999
+ */
+
+object WindowsApp {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    import org.apache.flink.api.scala._
+    val text = env.socketTextStream("localhost",9999)
+    text.flatMap(_.split(","))
+      .map((_,1))
+      .keyBy(0)
+      .timeWindow(Time.seconds(5))
+      .sum(1)
+      .print()
+      .setParallelism(1)
+
+    env.execute("WindowsApp")
+  }
+}
